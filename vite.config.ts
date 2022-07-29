1369dafe2400e73a75cd613583cb9f798815a766
Don Mbelembe <donmbelembe@gmail.com>
2022-07-29 16:30:30 +0100

    build

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        dir: 'docs',
+      }
+    }
+  },
   plugins: [vue()]
 })
